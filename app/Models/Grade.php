c6e47eaa7a0f1af4011160dca6cb929cb36af0da
Pascal Hurni <phi@pragmaticpurist.org>
2024-11-18 11:14:52 +0100

    Add model validation for Grade

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Watson\Validating\ValidatingTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Grade extends Model
 {
+	use ValidatingTrait;
+
+	protected $rules = [
+		'value' => 'required|decimal:0,1|min:1|max:6',
+	];
+
     protected $fillable = ['value'];
 
 }
