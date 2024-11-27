0f182ffda5e6fb4f0cebdf6c873e65cfcf238a5f
Pascal Hurni <phi@pragmaticpurist.org>
2024-11-27 14:22:12 +0100

    Update all routes to use the nested resource (course-grade)

--- app/Http/Controllers/GradeController.php
+++ app/Http/Controllers/GradeController.php
@@ -12,66 +12,66 @@ class GradeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Course $course)
     {
         $grades = Grade::fromUser(Auth::user());
-        return view('grades.index', ['grades' => $grades]);
+        return view('grades.index', ['grades' => $grades, 'course' => $course]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Course $course)
     {
-        return view('grades.create', ['grade' => new Grade(), 'courses' => Course::all()]);
+        return view('grades.create', ['grade' => new Grade(), 'course' => $course, 'courses' => Course::all()]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Course $course, Request $request)
     {
         $grade = new Grade($request->all());
         $grade->user()->associate(Auth::user());
         $grade->saveOrFail();
 
-        return redirect(route('grades.index'));
+        return redirect(route('courses.grades.index', $course));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Grade $grade)
+    public function show(Course $course, Grade $grade)
     {
-        return view('grades.show', compact('grade'));
+        return view('grades.show', compact('grade', 'course'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Grade $grade)
+    public function edit(Course $course, Grade $grade)
     {
         $courses = Course::all();
-        return view('grades.edit', compact('grade', 'courses'));
+        return view('grades.edit', compact('grade', 'course', 'courses'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Grade $grade)
+    public function update(Course $course, Grade $grade, Request $request)
     {
         $grade->updateOrFail($request->all());
 
-        return redirect(route('grades.show', $grade));
+        return redirect(route('courses.grades.show', [$course, $grade]));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Grade $grade)
+    public function destroy(Course $course, Grade $grade)
     {
         $grade->delete();
 
-        return redirect(route('grades.index'));
+        return redirect(route('courses.grades.index', $course));
     }
 }
--- resources/views/grades/create.blade.php
+++ resources/views/grades/create.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ route('grades.store') }}" method="post">
+<form action="{{ route('courses.grades.store', $course) }}" method="post">
     @csrf
     @include('grades.form')
 </form>
