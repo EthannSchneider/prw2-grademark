f25a2a3449d9ff82d799421317fa45eb1f8a8558
Pascal Hurni <phi@pragmaticpurist.org>
2025-01-02 23:36:59 +0100

    Add StudyPlan controller skeleton with its routing

---
+++ app/Http/Controllers/StudyPlanController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudyPlan;
+use Illuminate\Http\Request;
+
+class StudyPlanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StudyPlan $studyPlan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, StudyPlan $studyPlan)
+    {
+        //
+    }
+}
