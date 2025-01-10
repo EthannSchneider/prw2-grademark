dd5d729f86d1cf559b5068162512f837e45edd6a
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2025-01-10 15:26:56 +0100

    refactor: rewrite both student and manager middleware into one middleware able to handle all roles

---
+++ app/Http/Middleware/CheckTypeOfUser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Container\Attributes\CurrentUser;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+
+class CheckTypeOfUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string $role)
+    {
+
+        $user = Auth::user();
+        if ($user->type !== $role) {
+            abort(403, 'Accès interdit : rôle non autorisé.');
+        }
+
+        return $next($request);
+    }
+}
